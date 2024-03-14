d3e58dd4f983f0fa26978dbe0b7206aeb4a26a9f
Wei Wang <wangweisemail@163.com>
2024-03-14 15:01:25 +0800

    加点log

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,8 +1,10 @@
 // src/supabaseClient.js or .ts
 import { createClient } from '@supabase/supabase-js';
-console.log(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+console.log('%c [ createClient ]-6', 'font-size:13px; background:pink; color:#bf2f;',import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+  );
+  console.log('%c [ supabase ]-6', 'font-size:13px; background:pink; color:#bf2c9f;', supabase)
+
