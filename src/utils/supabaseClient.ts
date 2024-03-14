b24abebf25ffef7aae89cc1a4b5ac16a5dbbb0ff
Wei Wang <wangweisemail@163.com>
2024-03-14 16:09:21 +0800

    process.env.

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 // src/supabaseClient.js or .ts
 import { createClient } from '@supabase/supabase-js';
-console.log('%c [ createClient ]-6', 'font-size:13px; background:pink; color:#bf2f;',import.meta.env,import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+console.log('%c [ createClient ]-6', 'font-size:13px; background:pink; color:#bf2f;',import.meta.env,process.env,import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
